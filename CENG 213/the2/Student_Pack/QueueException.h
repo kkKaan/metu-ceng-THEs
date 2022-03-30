0511ee6ac5b35f4662d0161688dc0cf270068954
Kaan Karaçanta <72619096+kkKaan@users.noreply.github.com>
2022-03-30 11:33:16 +0300

    Add files via upload

---
+++ CENG 213/the2/Student_Pack/QueueException.h
@@ -0,0 +1,15 @@
+#ifndef QUEUEEXCEPTION_H
+#define QUEUEEXCEPTION_H
+
+// DO NOT CHANGE THIS FILE.
+
+class QueueException {
+public:
+    QueueException(std::string err) {
+        error = err;
+    }
+    
+    std::string error;
+};
+
+#endif //QUEUEEXCEPTION_H
