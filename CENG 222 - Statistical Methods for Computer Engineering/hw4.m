0ca25890a5a79ab99b421c804c2601362f1f4221
Kaan Karaçanta <72619096+kkKaan@users.noreply.github.com>
2023-08-13 23:36:34 +0300

    Update hw4.m

--- CENG 222 - Statistical Methods for Computer Engineering/hw4.m
+++ CENG 222 - Statistical Methods for Computer Engineering/hw4.m
@@ -8,11 +8,13 @@ l1 = 0.15;
 l2 = 0.01;
 
 for a = 1:numberOfRuns;
+    // calculate the probability density function for the number of cars and trucks
     f1 = exp(-lambdaAuto);
     f2 = exp(-lambdaTruck);
     weightAuto = 0;
     weightTruck = 0;
     
+    // calculate the number of cars and trucks
     i = 0;
     u = rand;
     while(u >= f1);
@@ -29,6 +31,7 @@ for a = 1:numberOfRuns;
     end;
     totalNoOfTruck = i;
     
+    // calculate the weight of the cars and trucks
     for n = 1:totalNoOfAuto;
         weightAuto = weightAuto + sum(-1/l1 * log(rand(a1,1)));
     end;
@@ -39,10 +42,12 @@ for a = 1:numberOfRuns;
     TotalWeight(a) = weightAuto + weightTruck;
 end;
 
+// calculate the probability that the total weight is greater than 200000
 p_est = mean(TotalWeight > 200000);
 expectedWeight = mean(TotalWeight);
 stdWeight = std(TotalWeight);
 
+// print the results
 fprintf('Estimated probability = %f\n', p_est);
 fprintf('Expected weight = %f\n', expectedWeight);
 fprintf('Standard deviation = %f\n',stdWeight); 
